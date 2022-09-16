0726c80949fdc2740a1af2949f2b553601b1f19b
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-09-16 18:30:25 +0000

    Did the code with proper style, explaining my thoughts at the moment! I ran Black, but I'm not entirely sure if I did it right or if there's any errors but it's okay I'll cope

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By: Katie
+# Date: September 16th, 2022
+# Displays "Hello people! This has proper style. My friend still has a parasocial relationship with Hank Anderson." with proper style!
+
+
+def main():
+    print("Hello people! This has proper style. My friend still has a parasocial relationship with Hank Anderson.")
+    print("I tried to copy paste the script of an entire Star Trek episode into my code but it didn't work. That made me sad.")
+
+
+if __name__ == "__main__":
+    main()
