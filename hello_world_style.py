9753407bcf8c3c43b15c7e5e55ee4ff126d524f3
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-09-17 03:48:22 +0000

    fixed header comment info

--- hello_world_style.py
+++ hello_world_style.py
@@ -2,7 +2,7 @@
 
 # Created By: Katie
 # Date: September 16th, 2022
-# Displays "Hello people! This has proper style. My friend still has a parasocial relationship with Hank Anderson." with proper style!
+# Displays "Hello people of the world! This has proper style. My friend still has a parasocial relationship with Hank Anderson." with proper style!
 
 
 def main():
